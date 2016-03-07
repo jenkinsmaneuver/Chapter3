6302376351f48b341b95ee7dbc24936916324fec
Jordan Podlucky <jordan.podlucky@ealas.org>
2016-03-07 14:49:05 -0800

    Hello world comment

--- Chapter3/src/Exercise3_2.java
+++ Chapter3/src/Exercise3_2.java
@@ -4,6 +4,7 @@ public class Exercise3_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println((char)('A' + Math.random() * 27));
+		//Hello World
 	}
 
 }
